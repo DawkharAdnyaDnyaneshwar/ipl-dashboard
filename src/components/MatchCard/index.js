e5349fc1cb14cdd103a078589e7918cd801e5cf5
Adnya <adnyadawkhar2005@gmail.com>
2025-11-11 12:26:49 +0000

    Completed react for Match Card component

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,25 @@
-// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {matchDetails} = props
+  const {competingTeam, competingTeamLogo, result, matchStatus} = matchDetails
+
+  return (
+    <li className="match-item">
+      <img
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+        className="competing-team-logo"
+      />
+      <p className="competing-team-name">{competingTeam}</p>
+      <p className="result">{result}</p>
+      <p
+        className={`match-status {matchStatus === 'Won' ? 'match-won' : 'match-lost'}`}
+      >
+        {matchStatus}
+      </p>
+    </li>
+  )
+}
+
+export default MatchCard
