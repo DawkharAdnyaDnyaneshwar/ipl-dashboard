67a842937987b81e9a28de3eb24846dde3a35617
Adnya <adnyadawkhar2005@gmail.com>
2025-11-11 10:11:41 +0000

    Completed react for Team Card component

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,1 +1,19 @@
-// Write your code here
+import './index.css'
+
+import {Link} from 'react-router-dom'
+
+const TeamCard = props => {
+  const {team} = props
+  const {id, name, teamImageUrl} = team
+
+  return (
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link">
+        <img src={teamImageUrl} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
