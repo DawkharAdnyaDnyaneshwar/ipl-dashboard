cabe91daae84d8c3b76f1c5756e85817670fe19d
Adnya <adnyadawkhar2005@gmail.com>
2025-11-11 09:39:10 +0000

    Completed rect for Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,56 @@
-// Write your code here
+import './index.css'
+
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TeamCard from '../TeamCard'
+
+class Home extends Component {
+  state = {teams: [], isLoading: true}
+
+  componentDidMount() {
+    this.getTeams()
+  }
+
+  getTeams = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = await response.json()
+    const updatedData = data.teams.map(eachInfo => ({
+      name: eachInfo.name,
+      id: eachInfo.id,
+      teamImageUrl: eachInfo.team_image_url,
+    }))
+
+    this.setState({teams: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {isLoading, teams} = this.state
+    return (
+      <div className="home-route-container">
+        <div className="teams-list-container">
+          <div className="ipl-dashboard-heading-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+              alt="ipl logo"
+              className="ipl-logo"
+            />
+            <h1 className="ipl-dashboard-heading">IPL Dashboard</h1>
+          </div>
+          {isLoading ? (
+            <div data-testid="loader" className="loader-container">
+              <Loader type="Oval" color="#ffffff" height={50} width={50} />
+            </div>
+          ) : (
+            <ul className="teams-list">
+              {teams.map(eachTeam => (
+                <TeamCard key={eachTeam.name} team={eachTeam} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
